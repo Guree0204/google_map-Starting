c9b079a91aeeb36cfff6559e75197e1cfa499343
Guree <guree0204@gmail.com>
2023-12-01 10:13:20 +0800

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyAn8Q2OxmB-A0TVeaf8UT4j2Jx0Ao_52QU";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
